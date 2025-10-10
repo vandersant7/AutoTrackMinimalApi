6ea447e69d14279f46e88f25b1b53422d1f5c18b
Evandro Santos <vandersant7@gmail.com>
2025-10-10 14:02:39 -0300

    refatora teste de administrador para usar enum Profile ao invés de string

--- Test/Domain/Entities/AdministratorTest.cs
+++ Test/Domain/Entities/AdministratorTest.cs
@@ -1,4 +1,6 @@
 using AutoTrackMinimalApi.Domain.Entity;
+using AutoTrackMinimalApi.Domain.DTOs.Enuns;
+
 
 namespace Test.Domain.Entities
 {
@@ -14,15 +16,18 @@ namespace Test.Domain.Entities
                 Id = 1,
                 Email = "admin@teste.com",
                 Password = "12345678",
-                Profile = "Admin"
+                Profile = Profile.Admin
             };
 
             // Act & Assert
             Assert.AreEqual(1, admin.Id);
             Assert.AreEqual("admin@teste.com", admin.Email);
             Assert.AreEqual("12345678", admin.Password);
-            Assert.AreEqual("Admin", admin.Profile);
-           
+            Assert.AreEqual<AutoTrackMinimalApi.Domain.DTOs.Enuns.Profile>(
+                            AutoTrackMinimalApi.Domain.DTOs.Enuns.Profile.Admin,
+                                admin.Profile
+            );
+
 
 
         }
